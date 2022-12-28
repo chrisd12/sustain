655fc85b68bf533e39589e9aa49d6fd79305a9d2
Chris <chris.dietrich12@gmail.com>
2022-12-28 18:23:59 +0100

    invest + metamask

--- frontend/src/component/Projects.js
+++ frontend/src/component/Projects.js
@@ -6,14 +6,35 @@ import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
 import { Button, CardActionArea, CardActions, requirePropFactory } from '@mui/material';
 import {useState, setValue} from 'react';
+import { web3, fundContract } from './contractInter';
 
+
+const getBalance = async (sliderValue) => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(sliderValue);
+
+  console.log(typeof(sliderValue));
+  try{
+      let balance = await fundContract().methods.fund().send({
+        from: accounts[0],
+        value: web3.utils.toWei(String(sliderValue), 'ether'),
+        gas:143224,
+      });
+
+      console.log(balance)
+  } catch(err) {
+      console.log(err)
+
+  }
+}
 
 export default function MultiActionAreaCard() {
-  const [sliderValue, setValue] = useState(1000);
+  const [sliderValue, setValue] = useState(0.05);
 
   const sliderChange = (event, newValue) => {
     setValue(newValue);
   };
+
 
   return (
     <Card sx={{ maxWidth: 345, margin: 2.5, boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)"}}>
@@ -37,13 +58,13 @@ export default function MultiActionAreaCard() {
       <CardActions>
       <Slider
         aria-label="Temperature"
-        defaultValue={1000}
+        defaultValue={0.05}
         valueLabelDisplay="auto"
         onChange={sliderChange}
-        step={200}
+        step={0.01}
         marks
-        min={200}
-        max={5000}
+        min={0.01}
+        max={1}
         />
       </CardActions>
       <CardContent>
@@ -52,7 +73,8 @@ export default function MultiActionAreaCard() {
         </Typography>
       </CardContent>
       <CardActions sx={{ display: "flex", justifyContent: "center", alignItems:"center" }}>
-        <Button size="medium" color="primary" style={{ backgroundColor: "lightgrey", width: "200px"  }}>
+        <Button onClick={() => getBalance(sliderValue)}
+        size="medium" color="primary" style={{ backgroundColor: "lightgrey", width: "200px"  }}>
           Invest
         </Button>
       </CardActions>
